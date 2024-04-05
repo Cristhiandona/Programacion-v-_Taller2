ce54472c592288c6a73e45799deb79d49597881c
cristhiandona <cristhiandona@gmail.com>
2024-04-04 20:57:07 -0500

    Cambios ejercicio 3

---
+++ Ejercicio4/ejercicio4.js
@@ -0,0 +1,11 @@
+//Estudiante: Cristhian Orlando Aguirre Cardona
+/*
+    Ejercicio 4: Este programa te ayuda a viajar. El programa pide al usuario si va a 
+    viajar con coche, tren, bicicleta o autobús. Si va en tren o autobús le recordará que 
+    lleve dinero para el billete.
+    Ejemplo. el cliente pide ir en tren, aparecerá el mensaje "lleva dinero para el 
+    ticket"
+*/
+
+
+
