a3017038731693c65082d14a76a8aeb1f70dcbeb
cristhiandona <cristhiandona@gmail.com>
2024-04-04 20:03:19 -0500

    cambios ejercicio 1

--- Ejercicio1/ejercicio1.js
+++ Ejercicio1/ejercicio1.js
@@ -1,8 +1,14 @@
 /*Ejercicio 1: Generar un código que determine si un numero es par o impar*/
 
-const numero1= 2;
+let numero1 = 23;
+let operacion = numero1 % 2;
 
-if (numero1 / 2){
-    
-    console.log ('El numero: + es par');
+if (operacion == 0){ 
+
+    console.log ('El numero ' + numero1 + ' es Par');
+
+}else{
+
+    console.log ('El numero ' + numero1 + ' es Impar');
+
 }
